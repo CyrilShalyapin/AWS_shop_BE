f568791de9039211afb8a3cd739d3b3b8f816e7e
KirylShaliapinTR <k.shaliapin@thomsonreuters.com>
2023-04-16 17:01:26 +0300

    Task-4

--- product-service/handler.js
+++ product-service/handler.js
@@ -1,5 +1,5 @@
-const mock = require('./mock.json');
-// import mock from './mock.json';
+const { v4 } = require('uuid');
+const AWS = require('aws-sdk');
 
 const headers = {
   'Access-Control-Allow-Methods': '*',
@@ -8,41 +8,103 @@ const headers = {
 };
 
 module.exports.getProductsList = async (event) => {
+  console.log(event);
   try {
-    const res = await Promise.resolve(mock);
-    console.log('res', res);
+    const dynamo = new AWS.DynamoDB.DocumentClient();
+    const { Items: products } = await dynamo.scan({ TableName: 'products' }).promise();
+    const { Items: stocks } = await dynamo.scan({ TableName: 'stocks' }).promise();
+    const joined = products.map((product, index) => {
+      const { count } = stocks[index];
+      return {...product, count};
+    });
     return {
       statusCode: 200,
       headers,
-      body: JSON.stringify(res),
+      body: JSON.stringify(joined),
     };
   } catch (error) {
     return {
       statusCode: 500,
       headers,
-      body: JSON.stringify({ message: error.message })
+      body: JSON.stringify({ message: error.message }),
     };
   }
 };
 
-module.exports.getProductsById = async (event) => {
-  const id = event.pathParameters.id;
-  const product = await Promise.resolve(mock.find(item => item.id === id));
-  
-  if (!product) {
+module.exports.getProductById = async (event) => {
+  console.log(event);
+  try {
+    const dynamo = new AWS.DynamoDB.DocumentClient();
+    const id = event.pathParameters.id;
+    const { Item: product } = await dynamo.get({
+      TableName: 'products',
+      Key: { id },
+    }).promise();
+    const { Item: stock } = await dynamo.get({
+      TableName: 'stocks',
+      Key: { id },
+    }).promise();
+    
+    if (!product) {
+      return {
+        statusCode: 404,
+        headers,
+        body: JSON.stringify({ message: 'Product not found' }),
+      };
+    }
+    
     return {
-      statusCode: 404,
+      statusCode: 200,
       headers,
-      body: JSON.stringify({ message: 'Product not found' })
+      body: JSON.stringify({ count: stock.count, ...product }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ message: error.message }),
     };
   }
-  
-  return {
-    statusCode: 200,
-    headers,
-    body: JSON.stringify(product),
-  };
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.createProduct = async event => {
+  console.log(event);
+  try {
+    const dynamo = new AWS.DynamoDB.DocumentClient();
+    const { count, description, price, title } = JSON.parse(event.body);
+
+    if ((!count || typeof count !== 'number') ||
+      (!description || typeof description !== 'string') ||
+      (!price || typeof price !== 'number') ||
+      (!title || typeof title !== 'string')) {
+      return {
+        statusCode: 400,
+        headers,
+        body: JSON.stringify({ message: 'Product Data Is Invalid' }),
+      };
+    }
+    const id = v4();
+
+    await dynamo.put({
+      TableName: 'products',
+      Item: { description, id, price, title },
+    }).promise();
+  
+    await dynamo.put({
+      TableName: 'stocks',
+      Item: { count, id },
+    }).promise();
+  
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ count, description, id, price, title }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ message: error.message }),
+    };
+  }
 };
